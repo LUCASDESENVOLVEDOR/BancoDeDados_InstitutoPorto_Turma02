d675091bf922363dadbb997911291fff46b5dd47
Lucas Aragão Feliciano <LUCAS_ARAGAO_FEL@HOTMAIL.COM>
2023-05-24 21:01:02 -0300

    Add files via upload

---
+++ SCRIPTS/DQL/SELECT_CLINICA.sql
@@ -0,0 +1,18 @@
+--Banco Clinica
+
+USE Clinica
+
+
+--Listar todos os veterinarios (nome de CRM) e a razão social  da clinica
+
+--Listar todas as raças que começam com a letra S (se nao existir nenhum no seu BD realizar
+--o cadastro)
+
+--Listar todos os tipos de pet que terminam com a letra O (se nao existir nenhum no seu BD realizar
+--o cadastro)
+
+--Listar todos os pets mostrandos os nomes de seus donos , adicionar apelido em cada coluna (ALIAS)
+
+--Listar todos os atendimentos mostrando o nome do veterinario que atendeu,
+-- o nome, a raça, e o tipo do Pet que foi atendido,
+--o nome do dono do pet e o nome da clinica onde o pet foi atendido.
---
+++ SCRIPTS/DQL/SELECT_VEICULOS.sql
@@ -0,0 +1,10 @@
+
+USE VEICULOS
+-- Banco Locadora
+
+--Listar todos os alugueis mostrando as datas de inicio e fim,
+ --o nome do cliente que alugou e nome do modelo do carro.
+
+-- Listar os alugueis de um determinado cliente mostrando
+ --   seu nome as datas de inicio e fim e o nome do modelo
+  --  do carro
